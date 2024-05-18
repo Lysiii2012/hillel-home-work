753ae1ba4ced84c1989e43cc7125a6471057eef2
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-18 16:56:58 +0300

    finish

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,12 +1,17 @@
 const userText = prompt('Введіть текст')
 const userOption = prompt('Які літери видаляємо?')
+let notF = ''
 
 function userAnswer(userText , userOption) {
    const textArray =  userText.split('');
    const filterArray = textArray.filter(item => item === ' ' || !userOption.includes(item));
-
+   const notFaund = textArray.filter(item => item !== ' ' && !userOption.includes(item));
+    
+   notF =  notFaund.join(' ')
    return filterArray.join('');
+  
 }
 
 const res = userAnswer(userText , userOption);
 console.log(res)
+console.log(`А цих літер не було в вашому тексті ${notF}`)
