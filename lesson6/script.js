0dfafc3d757f72859ba9e8807c63a4a544660946
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-21 13:47:38 +0300

    variant 2

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,10 +1,7 @@
 const array = [1, 3, 4, 6, 2, 5, 7];
 
-function removeElement(arr, index) {
-    if (index >= 0 && index < arr.length) {
-        arr.splice(index, 1);
-    }
-    return arr;
+function removeElement(arr, item) {
+    return array.filter(element => element !== item)
 }
 
 removeElement(array, 4);
