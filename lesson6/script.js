13c17fd8f7749396c93c1ac79fc8af0b15e73b99
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-21 13:31:36 +0300

    res

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,15 +1,10 @@
-const mixedArray = [1, 3, 'first', '2 aples', 20, null, undefined, 305, '22'  ]
+const array = [1, 3, 4, 6, 2, 5, 7];
 
-function filterNumber(arr) {
-    const ressFilter = arr.filter(item => typeof item === 'number' );
-    if (ressFilter.length === 0) {
-            return 0;
-        }
-
-    let resultSum = ressFilter.reduce((sum, current) => sum + current, 0);
-    const resultsAverage = resultSum / ressFilter.length;
-
-    return resultsAverage ;
+function removeElement(arr, index) {
+    if (index >= 0 && index < arr.length) {
+        arr.splice(index, 1);
+    }
+    return arr;
 }
 
-filterNumber(mixedArray)
+removeElement(array, 2);
