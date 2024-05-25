67ab4260c6462f37d103341ffc8cfccd11a62afb
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-25 13:04:04 +0300

    res

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,20 +1,33 @@
-const promptAnswer = prompt('Введіть число від 100')
+function resUserInput() {
+    let attemptsLeft = 10;
 
-function resUser(promptAnswer) {
-    const numberInput = parseFloat(promptAnswer);
+    for (let current = 0; current < attemptsLeft; current++) {
+        let promptAnswer = prompt('Введіть число більше 100');
+        let tryWrite = attemptsLeft - current - 1;
 
-    if (isNaN(numberInput)){
-        console.log('Ви ввели не число');
-        return false
+        if (promptAnswer === null || promptAnswer.trim() === "") {
+            console.log(`Давайте введемо ще раз, залишилося: ${tryWrite} спроб`);
+            continue;
+        }
+
+        let num = Number(promptAnswer);
+
+        if (isNaN(num)) {
+            console.log('Ви ввели не число. Спробуйте ще раз.');
+            continue;
+        }
+
+        if (num > 100) {
+            console.log(`Ви ввели число: ${num}`);
+            return;
+        } else if (num === 100) {
+            console.log('Потрібно не 100, а більше 100. Спробуйте ще раз.');
+        } else {
+            console.log(`Ви ввели число: ${num}, воно менше 100. Спробуйте ще раз, залишилося: ${tryWrite} спроб`);
+        }
     }
-
-    if (numberInput < 100){
-        console.log('Ви ввели число менше 100');
-    }
-    if (numberInput > 100){
-        console.log(`Ви ввели число : ${numberInput}`);
-        return 
-    }
-
+    
+    console.log("Дякуємо за увагу, Ваша остання цифра: " + promptAnswer);
 }
-resUser(promptAnswer)
+
+resUserInput();
