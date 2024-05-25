6062261a6c0630001e8f2adbed193e3f51530ccd
Людмила Ивченко <95207405+Lysiii2012@users.noreply.github.com>
2024-05-25 13:03:08 +0300

    res

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,11 +1,20 @@
-function carrent(f){
-    return function(a){
-        return function(b){
-            return a * b
-        }
+const promptAnswer = prompt('Введіть число від 100')
+
+function resUser(promptAnswer) {
+    const numberInput = parseFloat(promptAnswer);
+
+    if (isNaN(numberInput)){
+        console.log('Ви ввели не число');
+        return false
     }
+
+    if (numberInput < 100){
+        console.log('Ви ввели число менше 100');
+    }
+    if (numberInput > 100){
+        console.log(`Ви ввели число : ${numberInput}`);
+        return 
+    }
+
 }
-
-const res = carrent();
-
-console.log(res(5)(2))
+resUser(promptAnswer)
